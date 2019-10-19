188453a273c42c2a5cb28997ebbb872e2cc1003c
idris <idris@labrin.net>
2019-10-19 23:33:18 +0400

    functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navbar from './components/Navbar'
 // import User from "./components/User"
 
 function App() {
   return (
     <div className="container">
+      <Navbar/>
       <h4 className="header">App Components</h4>
+      
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Navbar(){
+    return (
+        <div>
+            <h3>My Nav</h3>
+        </div>
+
+    )
+}
+
+export default Navbar
