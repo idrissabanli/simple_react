02d2e2d26323ceef1b484b853fb49bcedb1c53f7
idris <idris@labrin.net>
2020-01-12 22:13:49 +0400

    router simple

--- src/App.js
+++ src/App.js
@@ -4,20 +4,36 @@ import Navbar from './components/Navbar';
 import Users from "./components/Users";
 import AddUser from './components/AddUser';
 // import Test from './components/Test'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 class App extends Component{
-
+  // <Navbar title="User App"/> 
+  //     <h4 className="header">App Components</h4>
+  //     <AddUser/>
+  //     <hr/>
+  //     <Users/>
   render(){
   return (
+    <Router>
     <div className="container">
   {/* User App Navbar function-unu icine gedir */}
     {/*  <Test test="deneme"/> */}
-      <Navbar title="User App"/> 
-      <h4 className="header">App Components</h4>
-      <AddUser/>
-      <hr/>
-      <Users/>
+    <Route exact path= "/" render = {
+      () => {
+        return <h3>Home Page</h3>
+      }
+      
+    }/>
+
+    <Route path= "/about" render = {
+      () => {
+        return <h3>About Page</h3>
+      }
+      
+    }/>
+      
     </div>
+    </Router>
   );
 }}
 
